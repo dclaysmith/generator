2223eb3a8fe40b35307da2529d65dea98736c99b
dclaysmith <dclaysmith@gmail.com>
2013-06-09 17:50:41 +0100

    Refactoring

--- src/dclaysmith/Generator.php
+++ src/dclaysmith/Generator.php
@@ -16,139 +16,139 @@ namespace dclaysmith;
 class Generator 
 {
 
-	/**
-	 * @var array
-	 */
-	private $connections;
+    /**
+     * @var array
+     */
+    private $connections;
 
-	/**
-	 * @var dclaysmith\Generator\Formatter
-	 */
-	private $formatter;
+    /**
+     * @var dclaysmith\Generator\Formatter
+     */
+    private $formatter;
 
-	/**
-	 * @var standard object
-	 */
-	private $config;	
+    /**
+     * @var standard object
+     */
+    private $config;    
 
-	/**
-	 * @param string $config (json);
-	 */
-	public function __construct($config) 
-	{
+    /**
+     * @param string $config (json);
+     */
+    public function __construct($config) 
+    {
 
-		/**
-		 * Read the config file
-		 */
-		$this->config = json_decode($config);
+        /**
+         * Read the config file
+         */
+        $this->config = json_decode($config);
 
-		if (json_last_error() != "") die("Error decoding JSON. Error code: ".json_last_error());
+        if (json_last_error() != "") die("Error decoding JSON. Error code: ".json_last_error());
 
-		$this->validateConfig();
+        $this->validateConfig();
 
-	}
+    }
 
     /**
      * @return void
      */
-	public function run() 
-	{
+    public function run() 
+    {
 
-		/**
-		 * Loop through the templates
-		 */
-		foreach ($this->config->templates as $templateConfig) {
-			$this->processTemplate($templateConfig);
-		}
+        /**
+         * Loop through the templates
+         */
+        foreach ($this->config->templates as $templateConfig) {
+            $this->processTemplate($templateConfig);
+        }
 
-	}
-	
+    }
+    
     /**
      * @return void
      */
-	private function processTemplate($templateConfig) 
-	{
+    private function processTemplate($templateConfig) 
+    {
 
-		$templateName		= $templateConfig->name;
+        $templateName       = $templateConfig->name;
 
-		$templatePath 		= $this->config->templateDirectory.DIRECTORY_SEPARATOR.$templateName.".php";
+        $templatePath       = $this->config->templateDirectory.DIRECTORY_SEPARATOR.$templateName.".php";
 
-		require_once($templatePath);
+        require_once($templatePath);
 
-		$className 			= "dclaysmith\\Generator\\Template\\".$templateName;		
+        $className          = "dclaysmith\\Generator\\Template\\".$templateName;        
 
-		$template 			= new $className;
+        $template           = new $className;
 
-		$template->tables 	= $this->getConnection($templateConfig->connection)->tables();
+        $template->tables   = $this->getConnection($templateConfig->connection)->tables();
 
-		if ($templateConfig->repeat == "table") {
+        if ($templateConfig->repeat == "table") {
 
-			foreach ($template->tables as $table) {
+            foreach ($template->tables as $table) {
 
-				if (!$output = $template->generate($table)) continue;
-				
-				$filename			= $template->filename($table->name);
-				$destination		= $this->config->outputDirectory.$templateConfig->outputDirectory.DIRECTORY_SEPARATOR.$filename;
+                if (!$output = $template->generate($table)) continue;
+                
+                $filename           = $template->filename($table->name);
+                $destination        = $this->config->outputDirectory.$templateConfig->outputDirectory.DIRECTORY_SEPARATOR.$filename;
 
-				if (file_exists($destination) && !$templateConfig->overwrite) continue;
+                if (file_exists($destination) && !$templateConfig->overwrite) continue;
 
-				$handle 			= (file_exists($destination)) ? fopen($destination, "w+") : fopen($destination, "x+");
-				fwrite($handle, $output);
-				fclose($handle);
+                $handle             = (file_exists($destination)) ? fopen($destination, "w+") : fopen($destination, "x+");
+                fwrite($handle, $output);
+                fclose($handle);
 
-				echo ". ".$filename."\n";
-			}
+                echo ". ".$filename."\n";
+            }
 
-		} else {
+        } else {
 
-				if (!$output 		= $template->generate()) continue;
-				
-				$filename			= $template->filename($table->name);
-				$destination		= $this->config->outputDirectory.$templateConfig->outputDirectory.DIRECTORY_SEPARATOR.$filename;
+                if (!$output        = $template->generate()) continue;
+                
+                $filename           = $template->filename($table->name);
+                $destination        = $this->config->outputDirectory.$templateConfig->outputDirectory.DIRECTORY_SEPARATOR.$filename;
 
-				if (file_exists($destination) && !$templateConfig->overwrite) continue;
-				
-				$handle 			= (file_exists($destination)) ? fopen($destination, "w+") : fopen($destination, "x+");
-				fwrite($handle, $output);
-				fclose($handle);
+                if (file_exists($destination) && !$templateConfig->overwrite) continue;
+                
+                $handle             = (file_exists($destination)) ? fopen($destination, "w+") : fopen($destination, "x+");
+                fwrite($handle, $output);
+                fclose($handle);
 
-				echo ". ".$filename."\n";
-		}
+                echo ". ".$filename."\n";
+        }
 
-	}
+    }
 
-	/**
-	 * @return void
-	 */
-	private function validateConfig() 
-	{
+    /**
+     * @return void
+     */
+    private function validateConfig() 
+    {
 
-	}
+    }
 
-	/**
-	 * @return dclaysmith\Generator\Connection
-	 */
-	private function getConnection($identifier) 
-	{
-		if (!$this->connections) {
-			$this->connections = array();
-			foreach ($this->config->connections as $connection) {
-				$this->connections[$connection->name] = new \dclaysmith\Generator\Connection\MySql(
-																			$connection->host,
-																			$connection->user,
-																			$connection->password,
-																			$connection->database); 
-			}			
-		}
-		return ($this->connections[$identifier]) ? $this->connections[$identifier] : false;
-	}
+    /**
+     * @return dclaysmith\Generator\Connection
+     */
+    private function getConnection($identifier) 
+    {
+        if (!$this->connections) {
+            $this->connections = array();
+            foreach ($this->config->connections as $connection) {
+                $this->connections[$connection->name] = new \dclaysmith\Generator\Connection\MySql(
+                                                                            $connection->host,
+                                                                            $connection->user,
+                                                                            $connection->password,
+                                                                            $connection->database); 
+            }           
+        }
+        return ($this->connections[$identifier]) ? $this->connections[$identifier] : false;
+    }
 
-	/**
-	 * @return dclaysmith\Generator\Formatter
-	 */
-	private function getFormatter()
-	{
-		return $this->formatter;
-	}
+    /**
+     * @return dclaysmith\Generator\Formatter
+     */
+    private function getFormatter()
+    {
+        return $this->formatter;
+    }
 }
 ?>
